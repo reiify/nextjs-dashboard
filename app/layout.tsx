2cac3b342eb6ea3eb66e05a77239bbacb1422f5d
reiify <dzecksonstorm@gmail.com>
2024-10-31 21:58:28 +0500

    ended chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Acme Dashboard',
+		default: 'Acme Dashboard',
+	},
+	description: 'The official Next.js Learn Dashboard built with App Router.',
+	metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
+			<Analytics />
 			<body className={`${inter.className} antialised`}>{children}</body>
 		</html>
 	);
